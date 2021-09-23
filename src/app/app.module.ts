d9b180ebda8473feba51a7537868f48c3b7930a4
badi3a <badiaabouhdid@gmail.com>
2021-09-23 16:49:10 +0200

    commit S2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainUserComponent } from './main-user/main-user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainUserComponent
   ],
   imports: [
     BrowserModule
