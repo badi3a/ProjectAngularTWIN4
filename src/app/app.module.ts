421a7a37d58b9cd08eb93c6527c0f1bf64ac971a
badi3a <badiaabouhdid@gmail.com>
2021-10-07 15:16:20 +0200

    commit S4.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainUserComponent } from './main-user/main-user.component';
 import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+import { ProductComponent } from './product/product.component';
 
-
+const ROUTES: Routes = [
+  {path: 'user', component: MainUserComponent},
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainUserComponent
+    MainUserComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
